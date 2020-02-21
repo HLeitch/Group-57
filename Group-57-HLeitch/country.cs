4e0ead08eb475cf26b891ca0e39fb9f04d8db4e4
Kr0M19701723 <61019903+Kr0M19701723@users.noreply.github.com>
2020-02-21 23:31:35 +0000

    Adding loop for inputs that are not "yes","no" or "abstain"

---
+++ Group-57-HLeitch/country.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace test
+{
+	public class Country
+
+	{
+		public bool participating = true;
+
+		//Permanant Variables
+		private float _population;
+		private bool _eurozoneMember;
+		public string name;
+		public float population {
+			get { return _population; }
+			set { _population = value; }
+
+
+		}
+
+		public Country(string n, float pop, bool euro)
+        {
+			name = n;
+			_population = pop;
+			_eurozoneMember = euro;
+
+
+        }
+
+		  
+
+	
+
+	}
+
+}
+
+
+
+
+
